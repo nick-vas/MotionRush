a03c8b89c2e62a47494d3512eb14c15af0841c52
Nikolaos-Nikiforos Vasilikiotis <n.vassilikiotis@gmail.com>
2015-03-15 12:51:37 +0000

    Working state. Play prompt missing. Leap not working.

--- Game.js
+++ Game.js
@@ -1,74 +1,91 @@
 MotionRush.Game = function (game) {
     // Game variable declaration
-    this.hovers;
-    this.hoversRequired;
-    this.score;
-    this.timeAvailable;
-    this.timeEvent;
-    this.gameover = false;
-    this.secondsElapsed;
-    this.box;
-    this.cursor;
+    this.hovers = 0; // times player hovers
+    this.score = 0; // times player completes hoversRequired
+    this.hoversRequired; // amount of hovers needed for image to change
+    this.hoversCall;
+    this.timer; // countDown value
+    this.countdown; // countDown call
+    this.overmessage;
+    this.box; // game body
     this.left = true;
     this.right = false;
+    this.hand1Visible = false;
+
+    this.controller = new Leap.Controller();
+
 };
 
 MotionRush.Game.prototype = {
 
     create: function () {
-
-        this.secondsElapsed = 0;
-        this.score = this.time.create(false);
-        this.score.loop(1000, this.updateScore, this);
-
-        timeAvailable = this.rnd.integerInRange(1, 4);
+        // controller setup
+        this.controller.connect();
+        this.controller.setBackground(true);
+        // game setup
+        this.timer = 2;
+        this.countdown = this.time.create(false);
+        this.countdown.loop(1000, this.updateTime, this);
         this.buildWorld();
     },
 
     buildWorld: function () {
 
         box = this.add.sprite(0, 0, 'box2_sheet', this.rnd.integerInRange(0, 3));
-        cursor = this.add.sprite(0, 0, 'pointer');
-        cursor.anchor.setTo(0.5, 0.5);
-        this.physics.enable(cursor, Phaser.Physics.ARCADE);
-        cursor.body.allowRotation = false;
-        hoversRequired = this.rnd.integerInRange(2, 5);
-        timeEvent = this.time.events.loop(Phaser.Timer.SECOND, this.decreaseTime);
-        this.score.start();
+        this.hoversRequired = this.rnd.integerInRange(2, 5);
+        this.hoversCall = this.add.bitmapText(10, 10, 'eightbitwonder', 'Hovers Required ' + this.hoversRequired, 20);
+        this.countdown.start();
+    },
+
+    gameRules: function () {
+        this.score++;
+        box.frame = this.rnd.integerInRange(0, 3);
+        this.hoversRequired = this.rnd.integerInRange(2, 5);
+        this.hovers = 0;
+        this.hoversCall.destroy();
+        this.hoversCall = this.add.bitmapText(10, 10, 'eightbitwonder', 'Hovers Required ' + this.hoversRequired, 20);
 
     },
 
-    updateScore: function () {
-        this.secondsElapsed++;
-    },
-
-    decreaseTime: function () {
-        this.timeAvailable--;
-
-        if (timeAvailable === 0) {
-            this.quitGame();
+    updateTime: function () {
+        this.timer--;
+        if (this.timer <= 0) {
+            this.timeUp();
         }
     },
 
-    quitGame: function () {
+    timeUp: function () {
+        // stop functions +
+        this.hoversCall.destroy();
+        this.countdown.destroy();
+        this.overmessage = this.add.bitmapText(this.world.centerX - 120, this.world.centerY - 55, 'eightbitwonder', 'TIME UP!\n\n' + this.score, 42);
+        this.overmessage.align = "center";
+        this.overmessage.inputEnabled = true;
+        this.overmessage.events.onInputDown.addOnce(this.quitGame, this);
+    },
+
+    quitGame: function (pointer) {
 
         this.state.start('Menu');
     },
 
     update: function () {
 
-        cursor.rotation = this.physics.arcade.moveToPointer(cursor, 1500, this.input.activePointer);
-
-        if (cursor.x > 500 && (this.right == false)) {
+        if (this.input.activePointer.x > 500 && (this.right == false)) {
             this.hovers++;
             this.left = false;
             this.right = true;
-        } else if (cursor.x < 500 && (this.left == false)) {
+        }
+
+        if (this.input.activePointer.x < 500 && (this.left == false)) {
             this.hovers++;
             this.left = true;
             this.right = false;
         }
 
+        if (this.hovers == this.hoversRequired) {
+            this.gameRules();
+        }
     }
 
 };
--- Preloader.js
+++ Preloader.js
@@ -6,11 +6,15 @@ MotionRush.Preloader.prototype = {
 
     preload: function () {
 
+        // font
+        this.load.bitmapFont('eightbitwonder', 'fonts/eightbitwonder.png', 'fonts/eightbitwonder.fnt');
+        // images, character
         this.load.image('menuBG', 'images/MotionRush_background.png');
         this.load.image('pointer', 'images/pointer.png');
         // spritesheets
         this.load.spritesheet('box2_sheet', 'images/box2_sheet.png', 1002, 1002);
         this.load.spritesheet('box4_sheet', 'images/box4_sheet.png', 1005, 1005);
+
 
     }, // preload
 
